9f72a97864676b8a5210c1873ddc63a6aea6be4f
EC2 Default User <ec2-user@ip-172-31-41-190.ap-northeast-1.compute.internal>
2023-01-25 14:03:50 +0000

    [Finish]4-11

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,15 +1,15 @@
 class ListsController < ApplicationController
+  def top
+  end
+  
   def new
     #Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
     @list = List.new
   end
 
   def create
-　　# 1. データを受け取り新規登録するためのインスタンス作成
     list = List.new(list_params)
-    # 2. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # 3. フラッシュメッセージを定義し、詳細画面へリダイレクト. 
     flash[:notice] = "投稿が成功しました"
     redirect_to list_path(list.id)
 
